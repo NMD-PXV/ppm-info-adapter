016db6fa35d8feb9c1ff91c315a9cb3fcb032646
pxvu97 <pxvu97@gmail.com>
2018-12-04 09:36:18 +0700

    Init

---
+++ src/test/java/com/dxc/ppm/nurseadapter/PpmNurseAdapterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dxc.ppm.nurseadapter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpmNurseAdapterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
